9604201d5e43a25b74aa539f85a0a231e56c094f
AntonSemenov <semenov.anton.a@gmail.com>
2026-07-27 13:23:30 +0300

    New Git

--- app/Views/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="footer-content">
-        <p>&copy; <?= date('Y') ?> EuroCargo ERP. All rights reserved.</p>
-        <div class="footer-links">
-            <a href="/help">Help</a>
-            <a href="/contact">Contact</a>
-        </div>
-    </div>
-</footer>
