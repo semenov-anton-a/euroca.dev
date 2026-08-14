8c0ee1a372596c81898a55421137d0f0fba3f3e6
AntonSemenov <semenov.anton.a@gmail.com>
2026-08-14 13:21:14 +0300

    Submenu test

--- app/Modules/Tests/Controllers/Tests.php
+++ app/Modules/Tests/Controllers/Tests.php
@@ -5,29 +5,22 @@ namespace App\Modules\Tests\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Services\View\MenuService;
+
 class Tests extends BaseController
 {
-
-    private function _getUrls()
-    {
-        $methods = $this->_getControllerMethods(__CLASS__);
-
-        $urls = [];
-
-        foreach ($methods as $method) 
-        {
-            if (str_starts_with($method, 'post_')) {
-                $urls['post'][$method] = base_url("tests/$method");
-            } else {
-                $urls['get'][$method] = base_url("tests/$method");
-            }
-        }
-        return $urls;
-    }
-
     public function index() 
     {      
         return $this->viewModule("index", [ "urls" => $this->_getUrls() ]);
+    }
+
+
+    public function getMenu()
+    {
+        $menuService = new MenuService();
+
+
+        dd($menuService->getAllMenu());
     }
 
 
@@ -55,6 +48,24 @@ class Tests extends BaseController
         $this->userService->logout();
         return redirect()->to('/');
     }
+
+private function _getUrls()
+    {
+        $methods = $this->_getControllerMethods(__CLASS__);
+
+        $urls = [];
+
+        foreach ($methods as $method) 
+        {
+            if (str_starts_with($method, 'post_')) {
+                $urls['post'][$method] = base_url("tests/$method");
+            } else {
+                $urls['get'][$method] = base_url("tests/$method");
+            }
+        }
+        return $urls;
+    }
+
 
     public function feature(){ return $this->viewModule('feature'); }
 
